f76539b50a7540d632faa87a8c558a2d3db04e16
chrisdionisius <chrisdionisius@gmail.com>
2023-06-24 23:35:39 +0700

    done refactoring account page

--- lib/app/modules/account/account_page.dart
+++ lib/app/modules/account/account_page.dart
@@ -3,136 +3,156 @@ import 'package:example_widget_testing/app/modules/account/components/highlight_
 import 'package:example_widget_testing/app/modules/account/components/information.dart';
 import 'package:example_widget_testing/app/modules/account/components/profile_buttons.dart';
 import 'package:example_widget_testing/app/modules/account/components/profile_pic.dart';
+import 'package:example_widget_testing/app/widgets/bottom_navbar.dart';
 import 'package:example_widget_testing/core/theme/colors.dart';
 import 'package:example_widget_testing/core/values/constant/post_json.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 import '../../widgets/post_thumbnail.dart';
 
 class AccountPage extends StatefulWidget {
   const AccountPage({super.key});
-  // add final void function here
-  // final void Function() onPostTabPressed;
 
   @override
   AccountPageState createState() => AccountPageState();
 }
 
 class AccountPageState extends State<AccountPage> {
+  int pageIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return getBody();
-  }
-
-  int pageIndex = 0;
-
-  Widget getBody() {
     var size = MediaQuery.of(context).size;
-    return SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-          Row(
-            children: const <Widget>[
-              ProfilePic(),
-              SizedBox(
-                width: 30,
-              ),
-              Expanded(
-                child: ProfileInformation(),
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          const AccountName(),
-          const SizedBox(
-            height: 10,
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 15),
-            child: ProfileButtons(),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          const HighlightList(),
-          const SizedBox(
-            height: 10,
-          ),
-          // add instagram account tab
-          Row(
-            children: [
-              InkWell(
-                onTap: () {
-                  setState(() {
-                    pageIndex = 0;
-                  });
-                },
-                child: Container(
-                  width: size.width / 2,
-                  height: 40,
-                  decoration: BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(
-                        color: Colors.white,
-                        width: (pageIndex == 0) ? 2 : 0,
-                      ),
-                    ),
-                  ),
-                  child: const Center(
-                    child: Icon(
-                      Icons.grid_on_outlined,
-                      color: white,
-                    ),
-                  ),
+
+    return Scaffold(
+      backgroundColor: black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        // remove back button
+        automaticallyImplyLeading: false,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Row(
+              children: const <Widget>[
+                Text(
+                  "yonoplamir",
+                  style: TextStyle(fontWeight: FontWeight.bold),
                 ),
-              ),
-              InkWell(
-                onTap: () {
-                  setState(() {
-                    pageIndex = 1;
-                  });
-                },
-                child: Container(
-                  width: size.width / 2,
-                  height: 40,
-                  decoration: BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(
-                        color: Colors.white,
-                        width: (pageIndex == 1) ? 2 : 0,
-                      ),
-                    ),
-                  ),
-                  child: const Center(
-                    child: Icon(
-                      // tagged account
-                      Icons.person_pin_outlined,
-                      color: white,
-                    ),
-                  ),
+                Icon(Icons.keyboard_arrow_down),
+              ],
+            ),
+            Row(
+              children: [
+                SvgPicture.asset(
+                  "assets/images/upload_icon.svg",
+                  width: 27,
                 ),
-              ),
-            ],
-          ),
-          Wrap(
-            spacing: 1.5,
-            runSpacing: 1.5,
-            children: List.generate(posts.length, (index) {
-              return InkWell(
-                onTap: () {
-                  Navigator.pushNamed(context, '/post');
-                },
-                child: PostThumbnail(
-                  key: Key(posts[index]['postImg']),
-                  imageUrl: posts[index]['postImg'],
+                const SizedBox(
+                  width: 10,
                 ),
-              );
-            }),
-          )
-        ],
+                // hamburger menu icon
+                const Icon(Icons.menu, size: 35),
+              ],
+            )
+          ],
+        ),
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: const <Widget>[
+                ProfilePic(),
+                SizedBox(width: 30),
+                Expanded(child: ProfileInformation()),
+              ],
+            ),
+            const SizedBox(height: 10),
+            const AccountName(),
+            const SizedBox(height: 10),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: ProfileButtons(),
+            ),
+            const SizedBox(height: 10),
+            const HighlightList(),
+            const SizedBox(height: 10),
+            // add instagram account tab
+            Row(
+              children: [
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      pageIndex = 0;
+                    });
+                  },
+                  child: Container(
+                    width: size.width / 2,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(
+                          color: Colors.white,
+                          width: (pageIndex == 0) ? 2 : 0,
+                        ),
+                      ),
+                    ),
+                    child: const Center(
+                      child: Icon(
+                        Icons.grid_on_outlined,
+                        color: white,
+                      ),
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      pageIndex = 1;
+                    });
+                  },
+                  child: Container(
+                    width: size.width / 2,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(
+                          color: Colors.white,
+                          width: (pageIndex == 1) ? 2 : 0,
+                        ),
+                      ),
+                    ),
+                    child: const Center(
+                      child: Icon(
+                        // tagged account
+                        Icons.person_pin_outlined,
+                        color: white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Wrap(
+              spacing: 1.5,
+              runSpacing: 1.5,
+              children: List.generate(posts.length, (index) {
+                return InkWell(
+                  onTap: () {
+                    Navigator.pushNamed(context, '/post');
+                  },
+                  child: PostThumbnail(
+                    key: Key(posts[index]['postImg']),
+                    imageUrl: posts[index]['postImg'],
+                  ),
+                );
+              }),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: const BottomNavbar(pageIndex: 4),
     );
   }
 }
--- lib/app/modules/post/post_page.dart
+++ lib/app/modules/post/post_page.dart
@@ -1,3 +1,4 @@
+import 'package:example_widget_testing/app/widgets/bottom_navbar.dart';
 import 'package:example_widget_testing/app/widgets/post_item.dart';
 import 'package:example_widget_testing/core/values/constant/post_json.dart';
 import 'package:example_widget_testing/core/values/constant/story_json.dart';
@@ -13,35 +14,37 @@ class PostPage extends StatefulWidget {
 class PostPageState extends State<PostPage> {
   @override
   Widget build(BuildContext context) {
-    return getBody();
-  }
-
-  int pageIndex = 4;
-
-  Widget getBody() {
-    return SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-          Column(
-            children: List.generate(
-              posts.length,
-              (index) {
-                return PostItem(
-                  postImg: posts[index]['postImg'],
-                  profileImg: posts[index]['profileImg'],
-                  name: posts[index]['name'],
-                  caption: posts[index]['caption'],
-                  isLoved: posts[index]['isLoved'],
-                  viewCount: posts[index]['commentCount'],
-                  likedBy: posts[index]['likedBy'],
-                  dayAgo: posts[index]['timeAgo'],
-                  userPhoto: profile,
-                );
-              },
-            ),
-          )
-        ],
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text("Post"),
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Column(
+              children: List.generate(
+                posts.length,
+                (index) {
+                  return PostItem(
+                    postImg: posts[index]['postImg'],
+                    profileImg: posts[index]['profileImg'],
+                    name: posts[index]['name'],
+                    caption: posts[index]['caption'],
+                    isLoved: posts[index]['isLoved'],
+                    viewCount: posts[index]['commentCount'],
+                    likedBy: posts[index]['likedBy'],
+                    dayAgo: posts[index]['timeAgo'],
+                    userPhoto: profile,
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: const BottomNavbar(pageIndex: 4),
     );
   }
 }
